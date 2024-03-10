d02b36a04d517680987e7c3f539c0a4d691c6f19
Diony Rosa <dhrosa@gmail.com>
2024-03-10 16:27:33 -0400

    Add logic for extracting field values and dunder method metadata from a class.

---
+++ src/udataclasses/dataclass.py
@@ -0,0 +1,39 @@
+from typing import TypeVar
+
+from .field import Field
+
+T = TypeVar("T")
+
+
+def dataclass(cls: type[T]) -> type[T]:
+    return cls
+
+
+class TransformSpec:
+    init: bool
+    repr: bool
+    eq: bool
+    fields: list[Field]
+
+    def __init__(
+        self, cls: type[T], *, init: bool = False, repr: bool = False, eq: bool = False
+    ) -> None:
+        self.init = init and ("__init__" not in cls.__dict__)
+        self.repr = repr and ("__repr__" not in cls.__dict__)
+        self.eq = eq and ("__eq__" not in cls.__dict__)
+
+        self.fields = []
+        for attr in cls.__dict__:
+            value = getattr(cls, attr)
+            field: Field
+            if isinstance(value, Field):
+                field = value
+                field.name = attr
+            else:
+                # Convert implicit field to an explicit one.
+                if callable(value) or attr.startswith("__"):
+                    # Ignore methods and dunder attributes
+                    continue
+                field = Field(attr, value)
+
+            self.fields.append(field)
--- src/udataclasses/field.py
+++ src/udataclasses/field.py
@@ -1,5 +1,42 @@
-class Field:
-    name: str
+from typing import Any
 
-    def __init__(self, name: str) -> None:
+
+class MissingType:
+    """Singleton type for MISSING value."""
+
+    def __repr__(self) -> str:
+        return "MISSING"
+
+    def __eq__(self, other: object) -> bool:
+        return other is self
+
+
+MISSING = MissingType()
+"""Placeholder for fields with no default value."""
+
+
+def field(default: Any = MISSING) -> "Field":
+    """Function for explicitly declaring a field."""
+    return Field(default=default)
+
+
+class Field:
+    """
+    Internal representation of a field provided by introspection routines.
+    Users should not directly instantiate this class.
+    """
+
+    name: str
+    default: Any
+
+    def __init__(self, name: str = "<UNSET>", default: Any = MISSING) -> None:
         self.name = name
+        self.default = default
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Field):
+            return self.name == other.name
+        return False
+
+    def __repr__(self) -> str:
+        return f"Field({self.name!r}, {self.default!r})"
