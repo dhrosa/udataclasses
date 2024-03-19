c502e761996198458fc55fe5652fe85daf0263a0
Diony Rosa <dhrosa@gmail.com>
2024-03-18 22:26:55 -0400

    Fix type stubs to properly use typing.dataclass_transform()
    Tests now type check without stealing type hints from dataclasses.
    
    Fixes #10

--- src/udataclasses/__init__.pyi
+++ src/udataclasses/__init__.pyi
@@ -1,14 +1,15 @@
-# Use type annotations from original dataclasses module.
+"""Stubs to make type checkers understand udataclasses in a similar way it understands dataclasses.
 
-from dataclasses import (
-    MISSING,
-    Field,
-    FrozenInstanceError,
-    dataclass,
-    field,
-    fields,
-    is_dataclass,
-)
+Inspired by: https://github.com/python/typeshed/blob/main/stdlib/dataclasses.pyi#L36-L44
+"""
+
+from collections.abc import Callable
+from typing import Any, Generic, TypeVar, dataclass_transform, overload
+
+from .constants import MISSING
+from .field import Field, FrozenInstanceError
+
+T = TypeVar("T")
 
 __all__ = [
     "Field",
@@ -19,3 +20,53 @@ __all__ = [
     "fields",
     "is_dataclass",
 ]
+
+@overload
+def dataclass(cls: type[T]) -> type[T]: ...
+@overload
+def dataclass(cls: None) -> type[T]: ...
+@dataclass_transform(field_specifiers=(field, Field))
+@overload
+def dataclass(
+    *,
+    init: bool = ...,
+    repr: bool = ...,
+    eq: bool = ...,
+    order: bool = ...,
+    unsafe_hash: bool = ...,
+    frozen: bool = ...,
+) -> Callable[[type[T]], type[T]]: ...
+
+# Overload that infers type from ``default``
+@overload
+def field(
+    *,
+    default: T,
+    init: bool = ...,
+    repr: bool = ...,
+    hash: bool | None = ...,
+    compare: bool = ...,
+) -> T: ...
+
+# Overload that infers type from ``default_factory``
+@overload
+def field(
+    *,
+    default_factory: Callable[[], T],
+    init: bool = ...,
+    repr: bool = ...,
+    hash: bool | None = ...,
+    compare: bool = ...,
+) -> T: ...
+
+# Overload with no default specified in any way.
+@overload
+def field(
+    *,
+    init: bool = ...,
+    repr: bool = ...,
+    hash: bool | None = ...,
+    compare: bool = ...,
+) -> Any: ...
+def fields(obj: object) -> tuple[Field, ...]: ...
+def is_dataclass(obj: object) -> bool: ...
