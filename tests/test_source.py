db443bea35def3d31f338e674da02e637fa8f958
Diony Rosa <dhrosa@gmail.com>
2024-03-22 12:53:13 -0400

    Refactor test_source.py to not need textwrap, so it can run inside MicroPython

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,7 +1,10 @@
-from textwrap import dedent
-
 import udataclasses.source as source
 from udataclasses.field import Field
+
+
+def assert_lines(actual: str, expected_lines: list[str]) -> None:
+    actual_lines = actual.splitlines()
+    assert actual_lines == expected_lines
 
 
 def test_init() -> None:
@@ -13,139 +16,152 @@ def test_init() -> None:
             Field("d", init=False),
         ]
     )
-    expected = """
-    def __init__(self, a, b=__dataclass_default_b, c=FACTORY_SENTINEL):
-        self._a = a
-        self._b = b
-        self._c = __dataclass_default_c() if c is FACTORY_SENTINEL else c
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __init__(self, a, b=__dataclass_default_b, c=FACTORY_SENTINEL):",
+            "    self._a = a",
+            "    self._b = b",
+            "    self._c = __dataclass_default_c() if c is FACTORY_SENTINEL else c",
+        ],
+    )
 
 
 def test_init_empty() -> None:
     out = source.init([])
-    expected = """
-    def __init__(self):
-        pass
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __init__(self):",
+            "    pass",
+        ],
+    )
 
 
 def test_getter() -> None:
     out = source.getter(Field("member"))
-    expected = """
-    @property
-    def member(self):
-        return self._member
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "@property",
+            "def member(self):",
+            "    return self._member",
+        ],
+    )
 
 
 def test_setter() -> None:
     out = source.setter(Field("member"))
-    expected = """
-    @member.setter
-    def member(self, value):
-        self._member = value
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "@member.setter",
+            "def member(self, value):",
+            "    self._member = value",
+        ],
+    )
 
 
 def test_deleter() -> None:
     out = source.deleter(Field("member"))
-    expected = """
-    @member.deleter
-    def member(self):
-        del self._member
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "@member.deleter",
+            "def member(self):",
+            "    del self._member",
+        ],
+    )
 
 
 def test_setter_frozen() -> None:
     out = source.setter(Field("member"), frozen=True)
-    expected = """
-    @member.setter
-    def member(self, value):
-        raise FrozenInstanceError('member')
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "@member.setter",
+            "def member(self, value):",
+            "    raise FrozenInstanceError('member')",
+        ],
+    )
 
 
 def test_deleter_frozen() -> None:
     out = source.deleter(Field("member"), frozen=True)
-    expected = """
-    @member.deleter
-    def member(self):
-        raise FrozenInstanceError('member')
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "@member.deleter",
+            "def member(self):",
+            "    raise FrozenInstanceError('member')",
+        ],
+    )
 
 
 def test_repr() -> None:
     out = source.repr([Field("a"), Field("b"), Field("c", repr=False)])
-    expected = """
-    def __repr__(self):
-        return f'{self.__class__.__name__}(a={self._a!r}, b={self._b!r})'
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __repr__(self):",
+            "    return f'{self.__class__.__name__}(a={self._a!r}, b={self._b!r})'",
+        ],
+    )
 
 
 def test_eq() -> None:
     out = source.eq([Field("a"), Field("b"), Field("c", compare=False)])
-    expected = """
-    def __eq__(self, other):
-        return (self._a, self._b,) == (other._a, other._b,)
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __eq__(self, other):",
+            "    return (self._a, self._b,) == (other._a, other._b,)",
+        ],
+    )
 
 
 def test_lt() -> None:
     out = source.lt([Field("a"), Field("b"), Field("c", compare=False)])
-    expected = """
-    def __lt__(self, other):
-        return (self._a, self._b,) < (other._a, other._b,)
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __lt__(self, other):",
+            "    return (self._a, self._b,) < (other._a, other._b,)",
+        ],
+    )
 
 
 def test_le() -> None:
     out = source.le([Field("a"), Field("b"), Field("c", compare=False)])
-    expected = """
-    def __le__(self, other):
-        return (self._a, self._b,) <= (other._a, other._b,)
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __le__(self, other):",
+            "    return (self._a, self._b,) <= (other._a, other._b,)",
+        ],
+    )
 
 
 def test_gt() -> None:
     out = source.gt([Field("a"), Field("b"), Field("c", compare=False)])
-    expected = """
-    def __gt__(self, other):
-        return (self._a, self._b,) > (other._a, other._b,)
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __gt__(self, other):",
+            "    return (self._a, self._b,) > (other._a, other._b,)",
+        ],
+    )
 
 
 def test_ge() -> None:
     out = source.ge([Field("a"), Field("b"), Field("c", compare=False)])
-    expected = """
-    def __ge__(self, other):
-        return (self._a, self._b,) >= (other._a, other._b,)
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __ge__(self, other):",
+            "    return (self._a, self._b,) >= (other._a, other._b,)",
+        ],
+    )
 
 
 def test_hash() -> None:
@@ -157,19 +173,21 @@ def test_hash() -> None:
             Field("d", compare=False),
         ],
     )
-    expected = """
-    def __hash__(self):
-        return hash((self._a, self._b, self._c,))
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __hash__(self):",
+            "    return hash((self._a, self._b, self._c,))",
+        ],
+    )
 
 
 def test_hash_empty() -> None:
     out = source.hash([])
-    expected = """
-    def __hash__(self):
-        return hash(())
-    """
-    expected = dedent(expected).strip()
-    assert out == expected
+    assert_lines(
+        out,
+        [
+            "def __hash__(self):",
+            "    return hash(())",
+        ],
+    )
